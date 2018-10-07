6502b35134f6ce8bd45de2dacaaa7c356b7af516
Drew Taylor <dretaylo@cisco.com>
2018-10-07 14:59:10 -0700

    Initial tests laid down

--- youtubeplayer/tests.py
+++ youtubeplayer/tests.py
@@ -1,7 +1,21 @@
 from django.test import TestCase
+from django.urls import resolve
+from django.http import HttpRequest
+
+from youtubeplayer.views import index
 
 # Create your tests here.
-class SmokeTest(TestCase):
+class HomePageTest(TestCase):
     
-    def test_bad_maths(self):
-        self.assertEqual(1 + 1, 3)
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, index)
+        
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = index(request)
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>DJ NoBangers</title>', html)
+        self.assertTrue(html.endswith('</html>'))
+
