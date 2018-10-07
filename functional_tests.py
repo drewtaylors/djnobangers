040cf8805314049c560edce2547a2055c3024f24
Drew Taylor <dretaylo@cisco.com>
2018-10-07 16:19:57 -0700

    Functional test now checks we can input a youtube item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -9,9 +11,29 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_can_retrieve_youtube_player(self):
+    def test_can_generate_youtube_list(self):
         self.browser.get('http://localhost:8000')
         self.assertIn('DJ NoBangers', self.browser.title)
+
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('DJ NoBangers', header_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'), 'Enter a youtube link'
+        )
+
+        inputbox.send_keys('Eminem')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+
+        self.assertTrue(
+            any(row.text == '1: Eminem' for row in rows)
+        )
+
         self.fail('Finish the test!')
 
 if __name__ == '__main__':
