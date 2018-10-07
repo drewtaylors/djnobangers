2bf83e96c5e7b175d146582ee60d5c1d087ff376
Drew Taylor <dretaylo@cisco.com>
2018-10-07 16:38:32 -0700

    refactor home page view to use a template

--- djnobangers/settings.py
+++ djnobangers/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'youtubeplayer.apps.YoutubeplayerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'youtubeplayer',
 ]
 
 MIDDLEWARE = [
--- youtubeplayer/tests.py
+++ youtubeplayer/tests.py
@@ -1,21 +1,8 @@
 from django.test import TestCase
-from django.urls import resolve
-from django.http import HttpRequest
-
-from youtubeplayer.views import index
 
 # Create your tests here.
 class HomePageTest(TestCase):
-    
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, index)
         
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = index(request)
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>DJ NoBangers</title>', html)
-        self.assertTrue(html.endswith('</html>'))
-
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'youtubeplayer/index.html')
