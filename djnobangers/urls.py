c289150512b94a7eb3588211ba0c4f859a0ad369
Drew Taylor <drewtaylor888@gmail.com>
2018-10-10 09:17:05 -0700

    Working unique lists

--- djnobangers/urls.py
+++ djnobangers/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from youtubeplayer import views as youtubeplayer_views
 
 urlpatterns = [
-    path('', include('youtubeplayer.urls')),
+    path('', youtubeplayer_views.index, name='index'),
+    path('youtubeplayer/', include('youtubeplayer.urls')),
     path('admin/', admin.site.urls),
 ]
