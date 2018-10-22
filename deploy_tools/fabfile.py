2025c364f02606461cc02dfbaffe5d764ec6c99d
Drew Taylor <drewtaylor888@gmail.com>
2018-10-21 20:59:29 -0700

    fixed errors in fabfile.py

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -2,11 +2,11 @@ import random
 from fabric.contrib.files import append, exists
 from fabric.api import cd, env, local, run
 
-REPO_URUL = 'https://github.com/drewtaylors/djnobangers.git'
+REPO_URL = 'https://github.com/drewtaylors/djnobangers.git'
 
 def deploy():
-    site_folder = f'/home/(env.user)/sites/(env.host)'
-    run(f'mkdir -p (site_folder)')
+    site_folder = f'/home/{env.user}/sites/{env.host}'
+    run(f'mkdir -p /home/{env.user}/sites/{env.host}')
     with cd(site_folder):
         _get_latest_source()
         _update_virtualenv()
@@ -14,13 +14,13 @@ def deploy():
         _update_static_files()
         _update_database()
 
-def _get_lastest_source()
+def _get_latest_source():
     if exists('.git'):
         run('git fetch')
     else:
-        run(f'git clone (REPO_URL) .')
+        run(f'git clone {REPO_URL} .')
     current_commit = local('git log -n 1 --format=%H', capture=True)
-    run(f'git reset --hard (current_commit)')
+    run(f'git reset --hard {current_commit}')
 
 def _update_virtualenv():
     if not exists('virtualenv/bin/pip'):
@@ -29,18 +29,16 @@ def _update_virtualenv():
 
 def _create_or_update_dot_env():
     append('.env', 'DJANGO_DEBUG_FALSE=y')
-    append('.env', f'SITENAME=(env.host)')
+    append('.env', f'SITENAME={env.host}')
     current_contents = run('cat .env')
     if 'DJANGO_SECRET_KEY' not in current_contents:
         new_secret = ''.join(random.SystemRandom().choices(
-            'abcdefghijklmnopqrstuvwxyz0123456789', k-50
+            'abcdefghijklmnopqrstuvwxyz0123456789', k=50
         ))
-        append('.env', f'DJANGO_SECRET_KEY=(new_secret)')
+        append('.env', f'DJANGO_SECRET_KEY={new_secret}')
 
 def _update_static_files():
     run('./virtualenv/bin/python manage.py collectstatic --noinput')
 
 def _update_database():
     run('./virtualenv/bin/python manage.py migrate --noinput')
-    
-    
