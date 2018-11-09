1323bd8e0c3f542c625d39ef4d1f904feb1ce0b7
Drew Taylor <drewtaylor888@gmail.com>
2018-11-09 13:50:27 -0800

    removed youtube api key

--- youtubeplayer/youtube.py
+++ youtubeplayer/youtube.py
@@ -8,13 +8,12 @@ import json
 
 class YouTubeClient():
     # API_KEY = os.environ.get('YOUTUBE_AUTH')
-    API_KEY = 'AIzaSyAIisdrMZFK-IvdgSbAFkKYi0ARdbK0YIM'
     BASE_URL = 'https://www.googleapis.com/youtube/v3/videos?'
 
     @staticmethod
     def get_youtube_title(id):
         title = ''
-        API_KEY = 'AIzaSyAIisdrMZFK-IvdgSbAFkKYi0ARdbK0YIM'
+        API_KEY = os.environ.get('YOUTUBE_AUTH')
         BASE_URL = 'https://www.googleapis.com/youtube/v3/videos?'
         response = requests.get(f'{BASE_URL}part=snippet&id={id}&key={API_KEY}')
         if response.status_code == 200:
